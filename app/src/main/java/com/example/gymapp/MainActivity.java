bf2e14403d3cb50618e05499dd18fd12a557442e
bo bo <172291671+bo-bo0@users.noreply.github.com>
2026-04-25 14:52:36 +0200

    add main menu

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.gymapp;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +15,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnVaiAggiungi;
+    AutoCompleteTextView mainMenu;
+    ImageButton mainMenuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +30,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnVaiAggiungi = findViewById(R.id.button);
-        btnVaiAggiungi.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddTrainingActivity.class);
-                startActivity(intent);
+
+            populateMainMenu();
+            mainMenuSetClickListener();
+            setMainMenuButtonOnClickListener();
+        }
+        private void readDaysList() {
+            String content;
+        }
+        private void populateMainMenu() {
+            String[] items = getResources().getStringArray(R.array.main_menu_options);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                    this,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    items
+            );
+
+            mainMenu = findViewById(R.id.mainMenu);
+            mainMenu.setAdapter(adapter);
+
+            mainMenu.setVisibility(INVISIBLE);
+        }
+        private void setMainMenuButtonOnClickListener() {
+            mainMenuButton = findViewById(R.id.mainMenuButton);
+
+            mainMenuButton.setOnClickListener(b -> {
+                mainMenu.showDropDown();
+            });
+        }
+        private void mainMenuSetClickListener() {
+            mainMenu.setOnItemClickListener((parent, view, position, id) -> {
+                String selected = (String) parent.getItemAtPosition(position);
+                changePage(selected);
+            });
+        }
+        private void changePage(String destinationPageName) {
+
+            Class<?> destClass;
+            switch(destinationPageName.toLowerCase()) {
+                case "aggiungi allenamento":
+                    destClass = AddTrainingActivity.class;
+                    break;
+
+                default:
+                    throw new IllegalArgumentException(destinationPageName + " does not exist.");
             }
-        });
+
+            Intent dest = new Intent(MainActivity.this, destClass);
+
+            startActivity(dest);
         }
     }
