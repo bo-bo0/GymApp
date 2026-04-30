287afb3bdcc03dffaa9e15ff13e29a149fff5b8b
spina <antoniomenadeo515@gmail.com>
2026-05-01 00:58:20 +0200

    add something of the page notes i create the graphics and i started the java code but in the code i write there are some errors i hope u can solve tht

--- app/src/main/java/com/example/gymapp/Notes.java
+++ app/src/main/java/com/example/gymapp/Notes.java
@@ -1,14 +1,49 @@
 package com.example.gymapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Notes extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        ImageButton btnDelete = findViewById(R.id.btn_delete);
+
+        final EditText editTitle = findViewById(R.id.edit_note_title);
+        final EditText editContent = findViewById(R.id.edit_note_content);
+
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTitle.setText("");
+                editContent.setText("");
+            }
+        });
+    }
+    private void saveNote()
+    {
+            FloatingActionButton fabSave = findViewById(R.id.fab_save);
+            fabSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String title = editTitle.getText().toString();
+                    String content = editContent.getText().toString();
+                    if (!title.isEmpty() && !content.isEmpty()) {
+                        saveNoteToDatabase(title, content);
+                        Toast.makeText(Notes.this, "Nota salvata", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
     }
 }
+
